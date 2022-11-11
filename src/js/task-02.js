a96b830a28db332d758a3c87db9a8898a2e1c897
KulykAlexander <kulikau@gmail.com>
2022-11-11 22:09:49 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
+const List = document.querySelector("#ingredients");
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (const ingredient of ingredients) {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  List.append(item);
+}
