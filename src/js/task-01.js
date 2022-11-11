b5f85e2bab4a2480619d01081ac4e244afcc2277
KulykAlexander <kulikau@gmail.com>
2022-11-11 20:42:43 +0200

    task-01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,10 @@
 const numberOfCategories = document.querySelectorAll("li.item");
-console.log(`Number of categories:`, numberOfCategories.length);
+console.log(`Number of categories: ${numberOfCategories.length}`);
+// console.log(numberOfCategories);
+
+numberOfCategories.forEach((element) => {
+  const category = element.firstElementChild;
+  console.log(`Category: ${category.textContent}`);
+  const categoryList = element.lastElementChild;
+  console.log(`Elements: ${categoryList.children.length}`);
+});
