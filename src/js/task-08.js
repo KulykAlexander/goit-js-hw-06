6ad143bf5585f9ac26bd1a873834892e9b5bdae1
KulykAlexander <kulikau@gmail.com>
2022-11-14 19:14:30 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+// export default
+// const user = {
+//   email,
+//   password,
+// };
+// const userEmail = document.querySelector("email");
+// const userPasswor = document.querySelector("password");
+// const btn = document.querySelector("button");
+const form = document.querySelector("form");
 
+form.addEventListener("submit", loginSubmit);
+function loginSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
