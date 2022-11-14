c172b62e44c8e45a543995924173df87a1d62ff8
KulykAlexander <kulikau@gmail.com>
2022-11-14 13:17:42 +0200

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,2 +1,6 @@
 const control = document.querySelector("#font-size-control");
 const textSize = document.querySelector("#text");
+
+control.addEventListener("input", (eve) => {
+  textSize.style.fontSize = eve.currentTarget.value + "px";
+});
