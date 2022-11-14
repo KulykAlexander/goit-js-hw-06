31b061338fb1f3c275d2bbe8d3047d65099d4431
KulykAlexander <kulikau@gmail.com>
2022-11-14 22:32:09 +0200

    task-09

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,15 +1,15 @@
 let counterValue = 0;
 const counterValueSpan = document.querySelector("span");
-const decrement = document.querySelector('[data-action="decrement]');
-const increment = document.querySelector('[data-action="increment]');
+const decrement = document.querySelector('button[data-action="decrement"]');
+const increment = document.querySelector('button[data-action="increment"]');
 
-const decrementClick = () => {
+function decrementClick() {
   counterValue -= 1;
   counterValueSpan.textContent = `${counterValue}`;
-};
-const incrementClick = () => {
+}
+function incrementClick() {
   counterValue += 1;
   counterValueSpan.textContent = `${counterValue}`;
-};
+}
 decrement.addEventListener("click", decrementClick);
 increment.addEventListener("click", incrementClick);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const widget = document.querySelector("body");
+const color = document.querySelector("span");
+const changeColor = document.querySelector("button");
+
+function chengeBackgroundColor() {
+  color.textContent = getRandomHexColor();
+  widget.style.backgroundColor = getRandomHexColor();
+}
+changeColor.addEventListener("click", chengeBackgroundColor);
